f70f0423ced4ed998899808866fb88a04d8b7a09
Rachelle25 <rachelleavory@gmail.com>
2022-07-01 13:59:51 +0000

    100-my_calculator.py

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+from sys import argv
+from calculator_1 import add, sub, mul, div
+if __name__ == '__main__':
+"""
+handles basic operators
+"""
+l_av = len(argv) - 1
+if l_av == 3:
+operator = argv[2]
+num_a = int(argv[1])
+num_b = int(argv[3])
+if operator == '+':
+res = add(num_a, num_b)
+print('{:d} + {:d} = {:d}'.format(num_a, num_b, res))
+exit(0)
+elif operator == '-':
+res = sub(num_a, num_b)
+print('{:d} - {:d} = {:d}'.format(num_a, num_b, res))                           
+exit(0)                                    
+
+elif operator == '*':
+res = mul(num_a, num_b)
+print('{:d} * {:d} = {:d}'.format(num_a, num_b, res))                                                                                  
+exit(0)                                    
+
+elif operator == '/':
+res = div(num_a, num_b)
+print('{:d} / {:d} = {:d}'.format(num_a, num_b, res))                                                                                                          
+exit(0)                                    
+else:
+print('invalid operator')
+exit(1)
+else:
+print('usage: ./100-my_calculator.py <a> <operator> <b>')
+exit(1)
