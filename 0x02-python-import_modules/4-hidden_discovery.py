db16efadab9aa9e4bc76e1e0da489fc2d7ebbe7e
Rachelle25 <rachelleavory@gmail.com>
2022-07-02 19:31:18 +0000

    4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,6 +2,6 @@
 import hidden_4
 
 if __name__ == '__main__':
-names = [name for name in dir(hidden_4) if name[:2] != "__"]
-for name in names:
-print(name)
+	names = [name for name in dir(hidden_4) if name[:2] != "__"]
+	for name in names:
+		print(name)
