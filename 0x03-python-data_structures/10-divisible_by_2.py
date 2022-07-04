c28496ebada0fb6b391fe12b5620f5d5284310e8
Rachelle25 <rachelleavory@gmail.com>
2022-07-04 20:19:53 +0000

    10-divisible_by_2.py

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+	bool_list = []
+	for i in my_list:
+		if i % 2 == 0:
+			bool_list.append(true)
+		else:
+			bool_list.append(false)
+	return bool_list
+
