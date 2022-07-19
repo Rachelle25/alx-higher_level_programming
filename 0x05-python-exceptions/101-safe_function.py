44be9eb03b1d0f7aa16bbc73da9dda32f34c3951
Rachelle25 <rachelleavory@gmail.com>
2022-07-19 19:15:55 +0000

    102-magic_calculation.py

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 import sys
 
-def safe_print_integer_err(value):
+def safe_function(fct, *args):
     try:
-        print("{:d}".format(value))
+        x = fct(*args)
     except Exception as e:
         sys.stderr.write("Exception: {}\n".format(e.args[0]))
-        return False
+        return None
     else:
-        return True
+        return x
