33bcab26240c7d63ffa6281f2f87a2a9839216a4
rachelle25 <rachelleavory@gmail.com>
2022-07-28 13:41:28 -0700

    add.integer

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-import calculator_1
+from calculator_1 import add, sub, mul, div
 if __name__ == '__main__':
 a = 10
 b = 5
-print(f"{a:d} + {b:d} = {calculator_1.add(a, b)}")
-print(f"{a:d} - {b:d} = {calculator_1.sub(a, b)}")
-print(f"{a:d} * {b:d} = {calculator_1.mul(a, b)}")
-print(f"{a:d} / {b:d} = {calculator_1.div(a, b)}")
+print("{0:d} + {1:d} = {calculator_1.add(a, b)}")
+print("{0:d} - {1:d} = {calculator_1.sub(a, b)}")
+print("{0:d} * {1:d} = {calculator_1.mul(a, b)}")
+print("{0:d} / {1:d} = {calculator_1.div(a, b)}")
