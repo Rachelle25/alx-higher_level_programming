621de2578c9c10bcfc2ef126e2dffff5f9ed6816
Rachelle25 <rachelleavory@gmail.com>
2022-08-01 22:35:36 +0000

    pypy

---
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -0,0 +1,6 @@
+from re import I
+
+#!/usr/bin/python3
+def magic_string(lst=[]):
+    lst += ["BestSchool"]
+    return ", ".join(lst)
---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""
+
+This is a module that containts a clas that avoids
+
+dynmaically created attributes
+
+"""
+
+
+
+
+
+class LockedClass():
+
+    """ locked class """
+
+    __slots__ = ['first_name']
