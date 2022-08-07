3e5d9a9efa767e0cf82dc124689e732358be829a
Rachelle25 <rachelleavory@gmail.com>
2022-08-07 15:09:54 +0000

    shapes

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+
+''' base.py
+
+'''
+
+
+
+
+
+import json
+
+
+
+
+
+class Base:
+
+    ''' Base class'''
+
+    __nb_objects = 0
+
+
+
+    def __init__(self, id=None):
+
+        ''' Base constructor'''
+
+        if id is not None:
+
+            self.id = id
+
+        else:
+
+            Base.__nb_objects += 1
+
+            self.id = Base.__nb_objects
+
+
+
+    @staticmethod
+
+    def to_json_string(list_dictionaries):
+
+        if not list_dictionaries:
+
+            return "[]"
+
+
+
+        return json.dumps(list_dictionaries)
---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,127 @@
+#!/usr/bin/python3
+
+'''square.py
+
+'''
+
+
+
+from models.rectangle import Rectangle
+
+
+
+
+
+class Square(Rectangle):
+
+    ''' Sqaure class'''
+
+
+
+    def __init__(self, size, x=0, y=0, id=None):
+
+        ''' Square constructor '''
+
+        super().__init__(size, size, x, y, id)
+
+
+
+    @property
+
+    def size(self):
+
+        ''' size getter'''
+
+        return self.width
+
+
+
+    @size.setter
+
+    def size(self, value):
+
+        ''' size setter'''
+
+        if not isinstance(value, int):
+
+            raise TypeError("width must be an integer")
+
+
+
+        if value <= 0:
+
+            raise ValueError("width must be > 0")
+
+
+
+        self.width = value
+
+        self.height = value
+
+
+
+    def __str__(self):
+
+        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
+
+
+
+    def update(self, *args, **kwargs):
+
+        if args:
+
+            attr = ['id', 'size', 'x', 'y']
+
+            for i in range(len(args)):
+
+                if attr[i] == 'size':
+
+                    setattr(self, "width", args[i])
+
+                    setattr(self, "height", args[i])
+
+                else:
+
+                    setattr(self, attr[i], args[i])
+
+        else:
+
+            for k, v in kwargs.items():
+
+                if k == "size":
+
+                    setattr(self, "width", v)
+
+                    setattr(self, "height", v)
+
+                else:
+
+                    if hasattr(self, k):
+
+                        setattr(self, k, v)
+
+
+
+    def to_dictionary(self):
+
+        """ returns the dictionary representation of a Square """
+
+        attr = ['id', 'size', 'x', 'y']
+
+        dict_sq = {}
+
+
+
+        for key in attr:
+
+            if key == 'size':
+
+                dict_sq[key] = getattr(self, 'width')
+
+            else:
+
+                dict_sq[key] = getattr(self, key)
+
+
+
+        return dict_sq
