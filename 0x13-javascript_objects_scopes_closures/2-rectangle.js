b017066d674bbac65874ef857829573dc2564175
Rachelle25 <rachelleavory@gmail.com>
2022-10-07 17:14:04 +0000

    rectangle

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,9 +1,10 @@
 #!/usr/bin/node
-
 class Rectangle {
   constructor (w, h) {
-    this.width = w;
-    this.height = h;
+    if (w >= 1 && h >= 1) {
+      this.width = w;
+      this.height = h;
+    }
   }
 }
 
